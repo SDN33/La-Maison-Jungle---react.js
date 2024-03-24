2f1fe33a06983414827cec4430bb5e4729c2475a
SDN33 <s.deinegri2@gmail.com>
2024-03-24 23:04:57 +0100

    react first page

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+import Cart from './Cart';
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
