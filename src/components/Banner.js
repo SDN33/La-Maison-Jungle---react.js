39231f9e994cc01675a2fdf94b6a1d80c0fd3a5e
SDN33 <s.deinegri2@gmail.com>
2024-03-25 19:36:51 +0100

    app react

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,14 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
-  return <h1>La maison jungle</h1>
+    const title = 'La maison jungle'
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1 className='lmj-title'>{title}</h1>
+        </div>
+    )
 }
 
 export default Banner
